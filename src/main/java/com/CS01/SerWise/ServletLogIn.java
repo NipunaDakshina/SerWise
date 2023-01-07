4873869ef248cedd9db8c30295ae4356714022e7
Nipuna <dakshinagn@gmail.com>
2023-01-07 10:18:31 +0530

    Initial commit

---
+++ src/main/java/com/CS01/SerWise/ServletLogIn.java
@@ -0,0 +1,93 @@
+package com.CS01.SerWise;
+
+import jakarta.servlet.*;
+import jakarta.servlet.http.*;
+import jakarta.servlet.annotation.*;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.sql.Connection;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+
+@WebServlet(name = "ServletLogIn", value = "/ServletLogIn")
+public class ServletLogIn extends HttpServlet {
+
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        PrintWriter out=response.getWriter();
+        String uname=(String) request.getParameter("uname");
+        String password=(String) request.getParameter("pw");
+        //response.sendRedirect("/BranchManager/Home/home.html");
+        Connection con=null;
+        Statement stmt=null;
+        ResultSet rs=null;
+        try {
+            con=DatabaseConnection.initializeDatabase();
+
+            String sql="select * from serwise.user where id='"+uname+"'";
+
+
+            stmt= con.createStatement();
+
+            rs=stmt.executeQuery(sql);
+
+            rs.next();
+            String dbUser=rs.getString("id");
+            String dbPass=rs.getString("password");
+            int dbRole=Integer.parseInt(rs.getString("role"));
+
+            if(uname.equals(dbUser) && password.equals(dbPass)){
+                    if(dbRole==5) {
+                        response.sendRedirect("/SerWise_war/BranchManager/Home/home.html");
+                    }
+            }
+            else {
+                //out.println(uname);
+                out.println(dbUser);
+                //out.println(password);
+                out.println(dbPass);
+                out.println(dbRole);
+                response.sendRedirect("/SerWise_war/Login/login.html");
+            }
+
+
+
+        } catch (SQLException e) {
+            //response.sendRedirect("/SerWise_war/Login/login.html");
+            throw new RuntimeException(e);
+            //out.println(uname);
+            //out.println(dbUser);
+            //out.println(password);
+            //out.println(dbPass);
+        } catch (ClassNotFoundException e) {
+
+            throw new RuntimeException(e);
+        }
+        finally {
+            close(con,stmt,rs);
+        }
+
+    }
+
+    private void close(Connection con, Statement stmt, ResultSet rs) {
+        try {
+            if(rs !=null){
+                rs.close();
+            }
+            if(stmt != null){
+                stmt.close();
+            }
+            if(con != null){
+                con.close();
+            }
+        }
+        catch (Exception exc){
+            exc.printStackTrace();
+        }
+    }
+
+
+}
+
---
+++ src/main/webapp/index.jsp
@@ -0,0 +1,35 @@
+
+<meta charset="UTF-8">
+<meta http-equiv="X-UA-Compatible" content="IE=edge">
+<meta name="viewport" content="width=device-width, initial-scale=1.0">
+<link rel="stylesheet" href="../../CSS/content.css">
+<link rel="stylesheet" href="../../CSS/navigation.css">
+<link rel="stylesheet" href="../../CSS/footer.css">
+<link rel="stylesheet" href="../../CSS/backgrount.css">
+<title>Home</title>
+</head>
+<body>
+<header class="navigation">
+    <img src="../../Assets/SerWise.png" class="navimg">
+    <table>
+        <tr>
+            <td><a href="../SerWise_war/Login/login.html"><button class="button">Logout</button></a></td>
+        </tr>
+    </table>
+</header>
+
+<div class="two-content-div">
+    <div>
+        <span class="title">SerWise</span><br>
+        <span> home page</span>
+    </div>
+</div>
+
+<footer class="footer">
+    <div class="center"><img src="../../Assets/SerWise.png" class="logo"></div>
+    <div class="center"><a href="#"> Contact Us </a> &nbsp|
+        &nbsp<a href="#"> About Us </a> &nbsp|
+        &nbsp <a href="#"> Legal Stuff </a></div>
+    <div class="center">All Rights Recieved</div>
+</footer>
+</body>
