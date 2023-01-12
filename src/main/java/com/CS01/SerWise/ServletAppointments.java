8a98a5a048798d0fd64e0b116548001563c8b6fd
NipunaDakshina <dakshinagn@gmail.com>
2023-01-12 14:11:01 +0530

    implement the resheduleappoinment page

--- src/main/java/com/CS01/SerWise/ServletAppointments.java
+++ src/main/java/com/CS01/SerWise/ServletAppointments.java
@@ -28,9 +28,16 @@ public class ServletAppointments extends HttpServlet {
         int branch_Id=(Integer) session.getAttribute("branchId");
         PrintWriter out=response.getWriter();
         String theCommand=request.getParameter("command");
-        out.println(theCommand);
+        //out.println(theCommand);
         //out.println(employeeId);
         //out.println(branchId);
+        String filterDate=null;
+        filterDate=request.getParameter("DATE");
+        int filterId=0;
+        filterId=Integer.parseInt(request.getParameter("ID"));
+
+
+
 
 
         if(theCommand.equals("LIST")){
@@ -70,9 +77,85 @@ public class ServletAppointments extends HttpServlet {
         }
 
         else if(theCommand.equals("UPDATE")){
+            List<Appoinmtent> appoinmtents=new ArrayList<>();
+            Connection connection=null;
+            Statement statement=null;
+            ResultSet resultSet=null;
+            try {
+                connection = DatabaseConnection.initializeDatabase();
+                String sql = "SELECT * FROM serwise.appoinment where Branch_Id='" + branch_Id + "'";
+                statement = connection.createStatement();
+                resultSet = statement.executeQuery(sql);
+                while (resultSet.next()){
+                    int appoinmentId=resultSet.getInt("Appoinment_Id");
+                    String date=resultSet.getString("Date");
+                    String time=resultSet.getString("Time");
+                    String description=resultSet.getString("Desription");
+                    int clientId=resultSet.getInt("Registered_Client_Id");
+                    int branchId=resultSet.getInt("Branch_Id");
+                    int vehicleId=resultSet.getInt("Vehicle_Id");
+                    Appoinmtent tempAppoinmtent=new Appoinmtent(appoinmentId,date,time,description,clientId,branchId,vehicleId);
+                    appoinmtents.add(tempAppoinmtent);
+                }
+
+                request.setAttribute("APPOINMENTS",appoinmtents);
+                RequestDispatcher rd=request.getRequestDispatcher("BranchManager/Appoinments/resheduleAppoinments.jsp");
+                rd.forward(request,response);
+
+            } catch (SQLException e) {
+                throw new RuntimeException(e);
+            } catch (ClassNotFoundException e) {
+                throw new RuntimeException(e);
+            } finally {
+                close(connection,statement,resultSet);
+            }
 
 
 
+        }
+
+        else if(filterDate!=null || filterId!=0){
+            List<Appoinmtent> appoinmtents=new ArrayList<>();
+            Connection connection=null;
+            Statement statement=null;
+            ResultSet resultSet=null;
+            String sql=null;
+            try {
+                connection = DatabaseConnection.initializeDatabase();
+
+
+                if (filterDate != null && filterId ==0) {
+                    sql = "SELECT * FROM serwise.appoinment where Branch_Id='" + branch_Id + "' AND Date='"+filterDate+"'";
+                }
+                if (filterDate == null && filterId !=0) {
+                    sql = "SELECT * FROM serwise.appoinment where Branch_Id='" + branch_Id + "' AND Appoinment_Id='"+filterId+"'";
+                }
+                if (filterDate != null && filterId !=0) {
+                    sql = "SELECT * FROM serwise.appoinment where Branch_Id='" + branch_Id + "' AND Date='"+filterDate+"' AND Appoinment_ID='"+filterId+"'";
+                }
+                statement = connection.createStatement();
+                resultSet = statement.executeQuery(sql);
+                while (resultSet.next()){
+                    int appoinmentId=resultSet.getInt("Appoinment_Id");
+                    String date=resultSet.getString("Date");
+                    String time=resultSet.getString("Time");
+                    String description=resultSet.getString("Desription");
+                    int clientId=resultSet.getInt("Registered_Client_Id");
+                    int branchId=resultSet.getInt("Branch_Id");
+                    int vehicleId=resultSet.getInt("Vehicle_Id");
+                    Appoinmtent tempAppoinmtent=new Appoinmtent(appoinmentId,date,time,description,clientId,branchId,vehicleId);
+                    appoinmtents.add(tempAppoinmtent);
+                }
+
+                request.setAttribute("APPOINMENTS",appoinmtents);
+                RequestDispatcher rd=request.getRequestDispatcher("BranchManager/Appoinments/resheduleAppoinments.jsp");
+                rd.forward(request,response);
+
+            } catch (SQLException e) {
+                throw new RuntimeException(e);
+            } catch (ClassNotFoundException e) {
+                throw new RuntimeException(e);
+            }
         }
 
 
